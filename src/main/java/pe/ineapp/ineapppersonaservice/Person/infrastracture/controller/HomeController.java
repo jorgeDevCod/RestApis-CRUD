b6ae6b2e6ad1c7a240fab6f4e9c62c9822beb272
jorgeDevCod <jorge2698.jgpm@gmail.com>
2022-11-13 15:41:35 -0500

    first commit

---
+++ src/main/java/pe/ineapp/ineapppersonaservice/Person/infrastracture/controller/HomeController.java
@@ -0,0 +1,15 @@
+package pe.ineapp.ineapppersonaservice.Person.infrastracture.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("")
+public class HomeController {
+
+    @RequestMapping("")
+    public String Index(@RequestParam(defaultValue = "Inviado") String param){
+        return "Bienvenido".concat(param);
+    }
+}
